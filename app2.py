d16fa212ecce0843d71ddc14e3118193814cbe18
unknown <nguyenbuuthinhgv@gmail.com>
2021-03-28 15:15:22 +0700

    Demo

--- app2.py
+++ app2.py
@@ -14,7 +14,7 @@ bot = Bot(ACCESS_TOKEN)
 # We will receive messages that Facebook sends our bot at this endpoint
 
 
-@app.route("/", methods=['GET', 'POST'])
+@app.route("/webhook", methods=['GET', 'POST'])
 def receive_message():
     if request.method == 'GET':
         """Before allowing people to message your bot, Facebook has implemented a verify token

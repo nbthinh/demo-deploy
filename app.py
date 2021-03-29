72810bd29a85f8d6fd01bdf7139fc5c8e299e32f
Nguyễn Bửu Thịnh <thinh.nguyen0908117036@hcmut.edu.vn>
2021-03-30 00:25:19 +0700

    .

--- app.py
+++ app.py
@@ -4,17 +4,9 @@ from flask import Flask, request
 from pymessenger.bot import Bot
 import os
 import psycopg2
-
-DB_HOST = 'ec2-23-21-229-200.compute-1.amazonaws.com'
-DB_NAME = 'd4k5c62e1ofteh'
-DB_USER = 'gljdcsupgxcqkl'
-DB_PASS = '0f13baa951eff711d3645bca70aeea92a8e471d9c7ea66bc2410d512046f9cfa'
-conn = psycopg2.connect(database=DB_NAME, user=DB_USER, password=DB_PASS, host=DB_HOST)
-sql ='''SELECT * FROM EMPLOYEE'''
-cur = conn.cursor()
-cur.execute(sql)
-demo = cur.fetchmany(2)
-# print(demo[0][0])
+import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import linear_kernel
 
 # Tham khảo tại đây: https://www.twilio.com/blog/2017/12/facebook-messenger-bot-python.html
 app = Flask(__name__)
@@ -40,10 +32,13 @@ def demo():
     sql ='''SELECT * FROM san_pham'''
     cur = conn.cursor()
     cur.execute(sql)
-    demo = cur.fetchall()
-    print(demo)
-    demo = demo[0][2]
-    return demo
+    ourdata = cur.fetchall()
+    # print(ourdata)
+    # ourdata = ourdata[0][2]
+    col_name = ["id_sanpham", "Loai_sp", "Ten_sp", "Mo_ta_sp", "Gia_tien", "Cau_hinh"]
+    df = pd.DataFrame(ourdata, columns=col_name)
+    # print(df)
+    return "Thay phun dep trai"
 
 @app.route("/", methods=['GET', 'POST'])
 def receive_message():
@@ -64,8 +59,31 @@ def receive_message():
         sql ='''SELECT * FROM san_pham'''
         cur = conn.cursor()
         cur.execute(sql)
-        ourdata = cur.fetchmany(2)
-        # print(ourdata)
+        ourdata = cur.fetchall()
+        col_name = ["id_sanpham", "Loai_sp", "Ten_sp", "Mo_ta_sp", "Gia_tien", "Cau_hinh"]
+        df = pd.DataFrame(ourdata, columns=col_name)
+        replace_by_space = ["/", ",", "-", ". ", "\n"]
+        replace_by_ignore = ["(", ")", "."]
+        added_col = []
+
+        for each_index in df.index:
+            val1 = df["Ten_sp"][each_index]
+            # val2 = df["Mo_ta_sp"][each_index]
+            # val3 = df["Gia_tien"][each_index]
+            # combine = val1 + " " + val2 + " " + str(val3)
+            combine = val1
+            for each_replace in replace_by_space:
+                combine = combine.replace(each_replace, " ")
+            for each_replace in replace_by_ignore:
+                combine = combine.replace(each_replace, "")
+            combine = combine.lower()
+            added_col.append(combine)
+        df["Process_data"] = added_col
+
+        vectorizer = TfidfVectorizer()
+        vectorizer.fit(df["Process_data"])
+        tfidf = vectorizer.transform(df["Process_data"])
+        feature_vector = tfidf.todense()
 
         output = request.get_json()
         for event in output['entry']:
@@ -76,11 +94,22 @@ def receive_message():
                     recipient_id = message['sender']['id']
                     text = message['message'].get('text')
                     if text:
-                        response_sent_text = get_message()
+                        # response_sent_text = get_message()
+                        
+                        returntext = "anh phong"
                         """
                         Tạm thời giải thuật recommendation system sẽ viết trong này
                         """
-                        send_message(recipient_id, text)
+                        _2tfidf = vectorizer.transform([text])
+                        sim_scores = linear_kernel(_2tfidf, tfidf)
+                        sim_scores_idx = list(enumerate(sim_scores))
+                        sim_scores = sorted(sim_scores_idx, key=lambda x: x[1], reverse=True)
+                        idx_choose = sim_scores[0]
+                        returntext = df['Ten_sp'].iloc(idx_choose)
+                        """
+                        Tạm thời giải thuật recommendation system sẽ viết trong này
+                        """
+                        send_message(recipient_id, returntext)
                     # if user sends us a GIF, photo,video, or any other non-text item
 
                     # if message['message'].get('attachments'):

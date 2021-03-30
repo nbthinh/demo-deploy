3724ee684ad597ce0ee1ef19f0f80e771bbf8d44
Nguyễn Bửu Thịnh <thinh.nguyen0908117036@hcmut.edu.vn>
2021-03-30 14:17:02 +0700

    gdf

--- app.py
+++ app.py
@@ -133,7 +133,7 @@ def receive_message():
                         # returntext = df['Ten_sp'].tolist()[idx_choose]
                         rec_product_name = df['Ten_sp'].iloc[idx_choose]
                         rec_product_desc = df['Mo_ta_sp'].iloc[idx_choose]
-                        rec_product_price = str(df['Gia_tien'].iloc[idx_choose])
+                        rec_product_price = str(int(df['Gia_tien'].iloc[idx_choose]))
                         returntext = rec_product_name + "\n" + rec_product_desc + "\n" + rec_product_price
                         """
                         Tạm thời giải thuật recommendation system sẽ viết trong này
